f61033055ece0f4b4a43961a3996d26514011eb3
Guillaume Besson <guillaume@besson.co>
2016-07-07 16:16:40 +0200

    Added Hls.js with basic quality selection

--- app/components/videoPlayer/index.js
+++ app/components/videoPlayer/index.js
@@ -1,14 +1,65 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+
+import Hls from 'hls.js';
 
 import styles from './styles.css';
 
-@connect()
+@connect((state) => ({
+  video: state.event.video,
+}))
 export class VideoPlayer extends React.Component {
+
+  static propTypes = {
+    video: PropTypes.object,
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      levels: [],
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.video && nextProps.video &&
+      this.props.video.m3u8_url !== nextProps.video.m3u8_url && this.hls) {
+      this.hls.destroy();
+    }
+    if (!nextProps.video) {
+      return;
+    }
+    this.hls = new Hls;
+    this.hls.attachMedia(this.refs.video);
+    this.hls.loadSource(nextProps.video.m3u8_url);
+    this.hls.on(Hls.Events.MANIFEST_PARSED, this.onManifestParsed);
+  }
+
+  onManifestParsed = (event, data) => {
+    this.setState({ levels: data.levels });
+    this.refs.video.play();
+  }
+
+  changeLevel = (level) => {
+    this.hls.nextLevel = level;
+  }
+
   render() {
+    if (!Hls.isSupported()) {
+      return <div className={styles.notSupported}>HLS is not supported</div>;
+    }
+
+    const levels = this.state.levels.map((level, i) => (
+      <button className={styles.level} onClick={() => this.changeLevel(i)} key={i}>{level.width} x {level.height}</button>
+    ));
+
     return (
       <div className={styles.container}>
-        VIDEO PLAYER IS HERE !
+        <div className={styles.levels}>
+          <button className={styles.level} onClick={() => this.changeLevel(-1)}>Auto</button>
+          {levels}
+        </div>
+        <video ref="video" controls />
       </div>
     );
   }
